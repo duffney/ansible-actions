d452400758d15e2304d5a8d942c02029539e7fd9
Josh Duffney <jduffney@stackoverflow.com>
2020-07-14 12:25:03 -0500

    added action and workflowfiles

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -6,4 +6,5 @@
 # ansible-vault --vault-password-file ~/.vault_pass.txt view ansible/ssh_key.txt.secret > ~/.ssh/id_rsa
 # chmod 0600 ~/.ssh/id_rsa
 
-ansible-playbook playbook.yml
+printenv
+#ansible-playbook playbook.yml
